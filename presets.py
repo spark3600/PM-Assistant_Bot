e3484139c1101fecc599d778416e50ae46f4a199
Aditya <90302830+spark3600@users.noreply.github.com>
2022-11-16 14:19:22 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,6 +1,6 @@
 
 class Presets(object):
-    WELCOME_TEXT = "Hello! <b>{}</b>\nType your movie name here and wait for 24 hours.🙂\nYour requested movie will be uploaded positively at @near_movies within 24 working hours.\n<code>Please note, If you're here to request a repository, invitation, script, internal support, etc. Kindly Get Lost!</code>\nKeep this in mind while texting, your message should not contain hate speech, violence or any kind of abuse.\n\nThank You."
+    WELCOME_TEXT = "Hello! <b>{}</b>\nType your query below and wait for my resporesponse.🙂\n<code>Please note, If you're here to request a repository, invitation, script, internal support, etc. Kindly Get Lost!</code>\nKeep this in mind while texting, your message should not contain hate speech, violence or any kind of abuse.\n\nThank You."
     USER_DETAILS = "<b>PM FROM:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
     PM_TXT_ATT = "<b>Message from:</b> {}\n<b>Name:</b> {}\n\n{}"
     PM_MED_ATT = "<b>Message from:</b> {} \n<b>Name:</b> {}"
